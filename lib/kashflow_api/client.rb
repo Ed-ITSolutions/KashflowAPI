28046c80422e0792c8b5d08a4fbaad45e607f35c
Adam Laycock <adam@arcath.net>
2013-09-16 13:49:13 +0100

    split up client code

--- lib/kashflow_api/client.rb
+++ lib/kashflow_api/client.rb
@@ -13,14 +13,15 @@ module KashflowApi
     end
     
     def call(api_call)
-      #"KashFlow/#{api_call.method}"
-      #result = @client.xml_call(api_call.method_sym, api_call)
       result = @client.call(api_call.method_sym, xml: api_call.xml)
-      raise "Incorrect username or password" if result.to_hash.first.last[:status_detail] == "Incorrect username or password"
-      raise "Your IP Address is not in the access list!" if result.to_hash.first.last[:status_detail] =~ /The IP address of .*? is not in the access list/
-      #raise api_call.xml if result.to_hash.first.last[:status] == "NO"
-      raise "Kashflow Error: #{result.to_hash.first.last[:status_detail]}" if result.to_hash.first.last[:status] == "NO"
+      handle_errors(result.to_hash)
       return result
+    end
+    
+    def handle_errors(hash)
+      raise "Incorrect username or password" if hash.first.last[:status_detail] == "Incorrect username or password"
+      raise "Your IP Address is not in the access list!" if hash.first.last[:status_detail] =~ /The IP address of .*? is not in the access list/
+      raise "Kashflow Error: #{result.to_hash.first.last[:status_detail]}" if hash.first.last[:status] == "NO"
     end
   end
 end
--- spec/core/client_spec.rb
+++ spec/core/client_spec.rb
@@ -6,8 +6,18 @@ describe KashflowApi::Client do
     config
   end
   
+  let(:instance) do
+    KashflowApi::Client.new(config_object)
+  end
+  
   it "should take a config object" do
     lambda { KashflowApi::Client.new("foo") }.should raise_exception
     lambda { KashflowApi::Client.new(config_object) }.should_not raise_exception
+  end
+  
+  it "should raise errors" do
+    lambda { instance.handle_errors([{stauts_detail: "Incorrect username or password"}]) }.should raise_exception
+    lambda { instance.handle_errors([{stauts_detail: "The IP address of 127.0.0.1 is not in the access list"}]) }.should raise_exception
+    lambda { instance.handle_errors([{status: "NO", stauts_detail: "There was a problem"}]) }.should raise_exception
   end
 end
